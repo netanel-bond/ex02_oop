6acd41d0ea18c20e0aaed6c24f1af068a880b9a0
Netanel Bondat <netanelbondar74@gmail.com>
2021-11-07 21:51:15 +0200

    added basic unfinished functions to Board

--- include/Board.h
+++ include/Board.h
@@ -1,9 +1,16 @@
 
+#include <string>
+#include <vector>
 
+using std::string;
+using std::vector;
 
 class Board {
 public:
+	Board();
 	void printBoard();
 private:
 
+	vector<string> m_board;
+	int m_boardSize = 0;
 };
--- src/Board.cpp
+++ src/Board.cpp
@@ -2,12 +2,32 @@
 
 #include "Board.h"
 #include <iostream>
+#include <fstream>
 
+using std::ifstream;
+using std::getline;
 using std::cout;
 
+Board::Board() 
+{
+    ifstream inputFile;
 
+    inputFile.open("Board.txt");
+    
+    string line;
+
+    inputFile >> m_boardSize;
+
+    int i = 0;
+    while (getline(inputFile, line))
+    {
+        m_board.push_back(line);
+    }
+}
 
 void Board::printBoard()
 {
-    
+    for (int i = 0; i < m_boardSize; i++)
+        cout << m_board[i];
+
 }
